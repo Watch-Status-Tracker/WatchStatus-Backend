40bebc502d86857c159ef4f74b26f04e4b741a8e
Numer <numerpriv@gmail.com>
2024-01-17 17:38:50 +0100

    ref: users seed

--- prisma/users.ts
+++ prisma/users.ts
@@ -5,15 +5,21 @@ export const seedUsers: Omit<User, 'id'>[] = [
     username: 'lajt',
     email: 'lajt@lajt',
     password: 'lajt',
+    title: 'Movie watcher',
+    favouriteGenre: '',
   },
   {
     username: 'kira',
     email: 'kira@kira',
     password: 'kira',
+    title: 'Movie watcher',
+    favouriteGenre: '',
   },
   {
     username: 'numer',
     email: 'numer@numer',
     password: 'numer',
+    title: 'Movie watcher',
+    favouriteGenre: '',
   },
 ];
