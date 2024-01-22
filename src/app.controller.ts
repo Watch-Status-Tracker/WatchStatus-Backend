b4ec521e1eca15ee18f77e160c2bae3b49e7c1bc
Numer <numerpriv@gmail.com>
2024-01-22 01:54:03 +0100

    x

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-import { hashPassword } from 'utils/bcrypt';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,12 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/')
-  async getHello(): Promise<any> {
-    const a = await hashPassword('numer');
-    const b = await hashPassword('kira');
-    const c = await hashPassword('lajt');
-    const d = await hashPassword('tester');
-    // return 'Hello World!';
-    return { numer: a, kira: b, lajt: c, tester: d };
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
